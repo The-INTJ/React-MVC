0f5cf52126034bac6a50795873fe3e44befbbf91
Drew Taylor <drew.taylor@perficient.com>
2020-07-24 11:16:01 -0400

    Add project files.

---
+++ ReactMVCcs/Scripts/React/src/App.jsx
@@ -0,0 +1,25 @@
+﻿import React, { Component } from 'react';
+
+
+
+
+class App extends Component {
+
+
+
+    render() {
+
+
+        return (
+            <div>
+                <p>Hello world!</p>
+            </div>
+
+            );
+    }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('react')); 
+
+//export default App;
